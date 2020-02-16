b06a56d6b9f36aa5f1fe684ab24db0fdd4851f1e
Andrew Struthers <struthers919@gmail.com>
2020-02-16 12:28:01 -0800

    Basic Scene with Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = new Vector3(Input.GetAxis("Horizontal"), 0);
+        rb.AddForce(input * speed);
+        
+    }
+}
